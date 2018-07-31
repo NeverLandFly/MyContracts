70be218131c2e3dd2093e3472bf4b0977d75ed12
NeverLandFly <jiajie_ying@163.com>
2018-07-31 01:32:13 -0700

    table modified

--- sports_betting/sports_betting.cpp
+++ sports_betting/sports_betting.cpp
@@ -6,5 +6,7 @@ class sports_betting : public eosio::contract{
         client_bets
         sports_betting(account_name self)
             :eosio::contract(self),
+             pools(_self, _self),
+             client
              
 }
