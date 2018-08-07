6013ab1bea88be420d956d1ddc9c7bacf2a36fb6
NeverLandFly <jiajie_ying@163.com>
2018-08-07 01:36:49 -0700

    order table

--- future/future.cpp
+++ future/future.cpp
@@ -1,22 +1,42 @@
 #include "future.hpp"
 #include <eosiolib/contract.hpp>
+#include <eosiolib/print.hpp>
 
 class future: public contract{
 public:
-    future(account_name self)
-    :contract(self),
-     current_description(_self, _self){}
+    future(account_name contract_account)
+    :contract(contract_account),
+     descriptions(_self, _self),
+     order_book(_self, _self){}
 
-    contract_description current_description;
+    contract_description descriptions;
+    orders order_book;
 
     //@abi action
-    void describe(const uint8_t multiple, const fixed_point128<8> price_tick){
-        auto info = current_description.emplace(_self, [&](auto& info){
-            info.id = current_description.available_primary_key();
+    void info(const uint8_t multiple, const uint32_t precision){
+        auto itr = descriptions.find( _self );
+        if (itr != descriptions.end()){
+            descriptions.erase(itr);
+        }
+        auto info = descriptions.emplace(_self, [&](auto& info){
+            info.contract_account = _self;
             info.multiple = multiple;
-//            info.price_tick = price_tick;
+            info.precision = precision;
+        });
+    }
+
+    //@abi action
+    void sendorder(account_name owner, uint32_t price/*, uint32_t volume, uint8_t type, uint64_t id*/){
+        auto itr = descriptions.find( _self );
+        int counter = itr->counter;
+        order_book.emplace(_self, [&](auto& o){
+            o.id = ((uint64_t)price<<32) + counter;
+            o.owner = owner;
+        });
+        descriptions.modify(itr, owner, [&](auto& info){
+            info.counter++;
         });
     }
 };
 
-EOSIO_ABI(future, (describe))
+EOSIO_ABI(future, (info)(sendorder))
--- future/future.hpp
+++ future/future.hpp
@@ -1,6 +1,5 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/multi_index.hpp>
-#include <eosiolib/fixedpoint.hpp>
 #include <string.h>
 
 using namespace eosio;
@@ -8,14 +7,25 @@ using namespace std;
 
 //@abi table description i64
 struct description {
-    uint128_t id;
+    account_name contract_account;
     uint16_t multiple;
-    uint64_t decimal_place;
-    uint64_t price_tick;
+    uint32_t precision;
+    uint32_t counter = 0;
 
-    auto primary_key() const { return id; }
-
-    EOSLIB_SERIALIZE(description, (id)(multiple)(decimal_place)(price_tick))
+    auto primary_key() const { return contract_account; }
+    EOSLIB_SERIALIZE(description, (contract_account)(multiple)(precision)(counter))
 };
 
 typedef multi_index<N(description), description> contract_description;
+
+//@abi table orders i64
+struct order{
+    uint64_t id;
+    uint8_t type;
+    uint32_t price;
+    account_name owner;
+
+    auto primary_key() const { return id; }
+    EOSLIB_SERIALIZE(order, (id)(type)(price)(owner))
+};
+typedef multi_index<N(order), order> orders;
