75865af2870fcc30453e99e23b6801b0da0bb663
NeverLandFly <jiajie_ying@163.com>
2018-08-08 01:33:59 -0700

    order deal

--- future/future.cpp
+++ future/future.cpp
@@ -1,6 +1,7 @@
 #include "future.hpp"
 #include <eosiolib/contract.hpp>
 #include <eosiolib/print.hpp>
+#include <string.h>
 
 class future: public contract{
 public:
@@ -13,30 +14,64 @@ public:
     orders order_book;
 
     //@abi action
-    void info(const uint8_t multiple, const uint32_t precision){
-        auto itr = descriptions.find( _self );
+    void info(uint32_t multiple, uint32_t precision,std::string underlying, uint64_t expiration){
+        /*auto itr = descriptions.find( _self );
         if (itr != descriptions.end()){
             descriptions.erase(itr);
-        }
+        }*/
         auto info = descriptions.emplace(_self, [&](auto& info){
             info.contract_account = _self;
             info.multiple = multiple;
             info.precision = precision;
+            info.underlying = underlying;
+            info.expiration = expiration;
         });
     }
 
     //@abi action
-    void sendorder(account_name owner, uint32_t price/*, uint32_t volume, uint8_t type, uint64_t id*/){
-        auto itr = descriptions.find( _self );
-        int counter = itr->counter;
-        order_book.emplace(_self, [&](auto& o){
-            o.id = ((uint64_t)price<<32) + counter;
-            o.owner = owner;
+    void insertorder(account_name owner, uint32_t price, uint32_t volume, uint8_t type) {
+        auto o = order{0, price, volume, type, owner};
+        auto itr = descriptions.find(_self);
+        auto ask1key = itr->ask1key;
+        auto bid1key = itr->bid1key;
+        auto counter = itr->counter;
+
+        if (ask1key == 0) {
+            addordertobook(o, counter);
+            descriptions.modify(itr, _self, [&](auto &info) {
+                info.counter++;
+
+            });
+        }
+
+        switch (type) {
+            case 0: // buy open
+            case 1: // buy close
+
+                break;
+            case 2: // sell open
+            case 3: // sell close
+
+                break;
+        }
+    }
+
+    void addordertobook(order& o, uint32_t counter){
+        order_book.emplace(_self, [&](auto& new_order){
+            new_order.id = ((uint64_t)o.price<<32) + counter;
+            new_order.price = o.price;
+            new_order.volume = o.volume;
+            new_order.type = o.type;
+            new_order.owner = o.owner;
         });
-        descriptions.modify(itr, owner, [&](auto& info){
-            info.counter++;
-        });
+    }
+
+    void removeorder(uint64_t orderid){
+        auto itr = order_book.find( orderid );
+        if (itr != order_book.end()){
+            order_book.erase(itr);
+        }
     }
 };
 
-EOSIO_ABI(future, (info)(sendorder))
+EOSIO_ABI(future, (info)(insertorder)(removeorder))
--- future/future.hpp
+++ future/future.hpp
@@ -1,6 +1,6 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/multi_index.hpp>
-#include <string.h>
+
 
 using namespace eosio;
 using namespace std;
@@ -8,24 +8,29 @@ using namespace std;
 //@abi table description i64
 struct description {
     account_name contract_account;
-    uint16_t multiple;
+    uint32_t multiple;
     uint32_t precision;
     uint32_t counter = 0;
+    string underlying;
+    uint64_t expiration;
+    uint64_t ask1key = 0;
+    uint64_t bid1key = 0;
 
     auto primary_key() const { return contract_account; }
-    EOSLIB_SERIALIZE(description, (contract_account)(multiple)(precision)(counter))
+    EOSLIB_SERIALIZE(description, (contract_account)(multiple)(precision)(counter)(underlying)(expiration)(ask1key)(bid1key))
 };
 
 typedef multi_index<N(description), description> contract_description;
 
-//@abi table orders i64
+//@abi table order i64
 struct order{
     uint64_t id;
-    uint8_t type;
     uint32_t price;
+    uint32_t volume;
+    uint8_t type;
     account_name owner;
 
     auto primary_key() const { return id; }
-    EOSLIB_SERIALIZE(order, (id)(type)(price)(owner))
+    EOSLIB_SERIALIZE(order, (id)(type)(price)(volume)(owner))
 };
 typedef multi_index<N(order), order> orders;
