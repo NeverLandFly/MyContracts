55b8dbfc317fae9eee86a85ca6bd29a5bcc5edd0
NeverLandFly <jiajie_ying@163.com>
2018-08-09 01:36:44 -0700

    order closed

--- future/future.cpp
+++ future/future.cpp
@@ -30,74 +30,120 @@ public:
 
     //@abi action
     void insertorder(account_name owner, uint32_t price, uint32_t volume, uint8_t type) {
-        auto o = order{0, price, volume, type, owner};
+        auto o = order{0, 0, price, volume, 0, type, owner};
         auto itr = descriptions.find(_self);
         auto ask1key = itr->ask1key;
         auto bid1key = itr->bid1key;
         auto counter = itr->counter;
+        auto rcounter = itr->rcounter;
 
-        if (bid1key == 0 && (type == 0 || type == 1)) {
-            auto id = addordertobook(o, counter);
-            descriptions.modify(itr, _self, [&](auto &info) {
-                info.counter++;
-                info.bid1key = id;
-            });
-            return;
-        }
-
-        if (ask1key == 0 && (type == 2 || type == 3)) {
-            auto id = addordertobook(o, counter);
-            descriptions.modify(itr, _self, [&](auto &info) {
-                info.counter++;
-                info.ask1key = id;
-            });
-            return;
-        }
-
-        auto biditr = order_book.find(bid1key);
         auto askitr = order_book.find(ask1key);
+        auto idx = order_book.template get_index<N(rid)>();
+        auto biditr = idx.find(bid1key);
+
 
         switch (type) {
             case 0: // buy open
             case 1: // buy close
-            if (askitr != order_book.end()){
-                if (price < askitr->price){
-                    auto id = addordertobook(o, counter);
-                    if (price > biditr->price)
-                        bid1key = id;
-                }else{
-                    /*auto current = volume;
-                    while (current > 0){
-                        if (current < askitr->get_unclosed()){
-                            current = 0;
-                            order_book.modify()
+                if (askitr != order_book.end()){
+                    if (price < askitr->price){
+                        auto id = addordertobook(o, counter, rcounter);
+                        if (price > biditr->price)
+                            bid1key = id;
+                    }else{
+                        auto current = volume;
+                        while (current > 0 && price >= askitr->price){
+                            if (current < askitr->get_unclosed()){
+                                current = 0;
+                                order_book.modify(askitr, _self, [&](auto &o) {
+                                    o.closedvolume += current;
+                                });
+                                break;
+                            }else{
+                                current -= askitr->get_unclosed();
+                                auto tempitr =askitr;
+                                if (askitr != order_book.end())
+                                    askitr++;
+                                order_book.erase(tempitr);
+                            }
                         }
-                    }*/
+                        if (current > 0)
+                        {
+                            o.volume = volume;
+                            o.closedvolume = volume - current;
+                            auto rid = addordertobook(o, counter, rcounter, true);
+                            bid1key = rid;
+                        }
+                    }
+                }else{
+                    auto rid = addordertobook(o, counter, rcounter, true);
+                    if (biditr != idx.end() && biditr->price < price){
+                        bid1key = rid;
+                    }else if (biditr == idx.end())
+                        bid1key = rid;
                 }
-            }
                 break;
             case 2: // sell open
             case 3: // sell close
-
+                if (biditr != idx.end()){
+                    print("found biditr.");
+                    if (price > biditr->price){
+                        auto id = addordertobook(o, counter, rcounter);
+                        if (price < askitr->price)
+                            ask1key = id;
+                    }else{
+                        auto current = volume;
+                        while (current > 0 && price <= biditr->price){
+                            if (current < biditr->get_unclosed()){
+                                idx.modify(biditr, _self, [&](auto &o) {
+                                    o.closedvolume += current;
+                                });
+                                current = 0;
+                                break;
+                            }else{
+                                current -= biditr->get_unclosed();
+                                auto tempitr = biditr;
+                                if (biditr != idx.begin())
+                                    biditr--;
+                                idx.erase(tempitr);
+                            }
+                        }
+                        if (current > 0)
+                        {
+                            o.volume = volume;
+                            o.closedvolume = volume - current;
+                            auto id = addordertobook(o, counter, rcounter);
+                            ask1key = id;
+                        }
+                    }
+                }else{
+                    auto id = addordertobook(o, counter, rcounter);
+                    if (askitr != order_book.end() && askitr->price > price){
+                        ask1key = id;
+                    }else if (askitr == order_book.end())
+                        ask1key = id;
+                }
                 break;
         }
         descriptions.modify(itr, _self, [&](auto &info) {
             info.counter++;
+            info.rcounter--;
             info.bid1key = bid1key;
             info.ask1key = ask1key;
         });
     }
 
-    uint64_t addordertobook(order& o, uint32_t counter){
-        auto id = ((uint64_t)o.price<<32) + counter;
-        order_book.emplace(_self, [&](auto& new_order){
-            new_order.id = id;
+    uint64_t addordertobook(order& o, uint32_t counter, uint32_t rcounter, bool retR = false){
+        auto new_order = order_book.emplace(_self, [&](auto& new_order){
+            new_order.id = ((uint64_t)o.price<<32) + counter;
+            new_order.rid = ((uint64_t)o.price<<32) + rcounter;
             new_order.price = o.price;
             new_order.volume = o.volume;
+            new_order.closedvolume = o.closedvolume;
             new_order.type = o.type;
             new_order.owner = o.owner;
         });
-        return id;
+        return retR?new_order->rid:new_order->id;
     }
 
     void removeorder(uint64_t orderid){
@@ -106,6 +152,20 @@ public:
             order_book.erase(itr);
         }
     }
+
+    void getridindex(){
+        auto idx = order_book.template get_index<N(rid)>();
+        auto biditr = idx.begin();
+        auto enditr = idx.end();
+        auto itr = descriptions.find(_self);
+        auto bid1key = itr->bid1key;
+        auto keyitr = idx.find(bid1key);
+        print("begin: ", biditr->rid, ", end: ", enditr->rid, ", key: ", keyitr->rid,"\n");
+        while (biditr != idx.end()){
+            print("current: ", biditr->rid, "\n");
+            biditr++;
+        }
+    }
 };
 
-EOSIO_ABI(future, (info)(insertorder)(removeorder))
+EOSIO_ABI(future, (info)(insertorder)(removeorder)(getridindex))
--- future/future.hpp
+++ future/future.hpp
@@ -11,6 +11,7 @@ struct description {
     uint32_t multiple;
     uint32_t precision;
     uint32_t counter = 0;
+    uint32_t rcounter = (2<<29) - 1;
     string underlying;
     uint64_t expiration;
     uint64_t ask1key = 0;
@@ -18,7 +19,7 @@ struct description {
     uint32_t lastprice = 0;
 
     auto primary_key() const { return contract_account; }
-    EOSLIB_SERIALIZE(description, (contract_account)(multiple)(precision)(counter)(underlying)(expiration)(ask1key)(bid1key)(lastprice))
+    EOSLIB_SERIALIZE(description, (contract_account)(multiple)(precision)(counter)(rcounter)(underlying)(expiration)(ask1key)(bid1key)(lastprice))
 };
 
 typedef multi_index<N(description), description> contract_description;
@@ -26,6 +27,7 @@ typedef multi_index<N(description), description> contract_description;
 //@abi table order i64
 struct order{
     uint64_t id;
+    uint64_t rid;
     uint32_t price;
     uint32_t volume;
     uint32_t closedvolume;
@@ -34,6 +36,9 @@ struct order{
 
     auto primary_key() const { return id; }
     auto get_unclosed() const {return volume - closedvolume;}
-    EOSLIB_SERIALIZE(order, (id)(price)(volume)(closedvolume)(type)(owner))
+    auto by_rid() const {return rid;}
+    EOSLIB_SERIALIZE(order, (id)(rid)(price)(volume)(closedvolume)(type)(owner))
 };
-typedef multi_index<N(order), order> orders;
+typedef multi_index<N(order), order,
+    indexed_by<N(rid), const_mem_fun<order, uint64_t , &order::by_rid>>
+> orders;
